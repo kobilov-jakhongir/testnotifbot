5e7e8ab21e0b8c86a4e9457259dc9470db5a266f
Jakhongir <kobilov.jakhongir@gmail.com>
2021-10-01 02:53:51 +0500

    first commit

---
+++ const.js
@@ -0,0 +1,20 @@
+const commands = `   
+/start - Перезапустить бота 
+/help - помощь 
+/course - курс
+`
+const text1 = `
+Текст для проверки обработчика и <a href="https://youtube.com/">ссылка</a>
+`
+
+const text2 = `
+Текст для проверки обработчика и <a href="https://facebook.com/">ссылка</a>
+`
+
+const text3 = `
+Текст для проверки обработчика и <a href="https://gmail.com/">ссылка</a>
+`
+module.exports.commands = commands
+module.exports.text1 = text1
+module.exports.text2 = text2
+module.exports.text3 = text3
